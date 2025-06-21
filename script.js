7eaf22658298abe65dc961d4a31c2440a53db7be
FridasamKarimi <muriithifridasam@gmail.com>
2025-06-21 06:43:04 +0300

    modals

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.ovelay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.show-modal');
+console.log(btnOpenModal);
